01c90650970f0f2431b7f6655b14e97d7d4ed187
salman zafar <salmanzafar949@gmail.com>
2017-10-11 16:33:09 +0500

    Made some Changes in the Details Views

--- Test/models.py
+++ Test/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 class Album(models.Model):
     artist      = models.CharField(max_length=20)
     album_title = models.CharField(max_length=30)
     genre       = models.CharField(max_length=10)
     album_logo  = models.CharField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse('ListAlbums',kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.album_title + '-' + self.artist
